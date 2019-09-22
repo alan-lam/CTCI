4df2000656d465e3237e08dec8e5726de4471278
ntrllog <alanlam00@gmail.com>
2019-09-22 15:04:06 -0700

    Q1.4

--- Ch. 1/PalindromePermutation.py
+++ Ch. 1/PalindromePermutation.py
@@ -1,25 +1,25 @@
 # Check if a string is a permutation of a palindrome
 
-# Dictionary O(N)
-# Palindrome has at most 1 odd count character
-def palindromePermutation(s):
+# Dictionary O(n)
+# A palindrome has at most 1 odd count character
+def palindromePermutation(check_str):
     d = {}
-    odd = 0
-    for c in s:
-        if d.get(c) is None:
-            d[c] = 1
-        else:
-            d[c] += 1
-    for x in d:
-        if d[x] % 2 == 1:
-            odd += 1
-        else:
-            if odd > 0:
-                odd -= 1
-    if odd < 2:
-        return True
-    return False
 
-s = input()
-print(palindromePermutation(s))
+    for char in check_str:
+        if char in d:
+            d[char] += 1
+        else:
+            d[char] = 1
+
+    found_odd = False
+    for char in d:
+        if d[char] % 2 == 1:
+            if found_odd == True:
+                return False
+            else:
+                found_odd = True
+    return True
+
+test_str = input('Enter string: ')
+print(palindromePermutation(test_str))
 
