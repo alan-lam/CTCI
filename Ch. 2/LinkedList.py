8d4b2f1bc3f2c761424d24793546d14e2dec8575
ntrllog <alanlam00@gmail.com>
2019-09-22 20:47:26 -0700

    linked list

--- Ch. 2/LinkedList.py
+++ Ch. 2/LinkedList.py
@@ -21,13 +21,13 @@ class LinkedList():
 
     def append(self, value):
         curr = self.head
-        while curr != None:
+        while curr.next is not None:
             curr = curr.next
         curr.next = Node(value)
 
     def printList(self):
         curr = self.head
-        while curr != None:
+        while curr is not None:
             print(curr.value)
             curr = curr.next
 
@@ -43,22 +43,22 @@ class DoublyLinkedList():
 
     def append(self, value):
         curr = self.head
-        while curr != None:
+        while curr.next is not None:
             curr = curr.next
         curr.next = Node(value)
         curr.next.prev = curr
 
     def printList(self):
         curr = self.head
-        while curr != None:
+        while curr is not None:
             print(curr.value)
             curr = curr.next
 
     def printListReverse(self):
         curr = self.head
-        while curr.next != None:
+        while curr.next is not None:
             curr = curr.next
-        while curr != None:
+        while curr is not None:
             print(curr.value)
             curr = curr.prev
 
